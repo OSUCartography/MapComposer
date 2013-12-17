ed5534106879436365f326ed522e86b9437ad377
Bernhard Jenny <jennyb@geo.oregonstate.edu>
2013-12-16 17:13:29 -0800

    Test for serialization of tiles.

--- src/test/java/edu/oregonstate/carto/tilemanager/SQLiteCacheTest.java
+++ src/test/java/edu/oregonstate/carto/tilemanager/SQLiteCacheTest.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -22,25 +24,25 @@ import static org.junit.Assert.*;
  * @author nick
  */
 public class SQLiteCacheTest {
-    
+
     private SQLiteCache sqliteCache;
-    
+
     public SQLiteCacheTest() {
         sqliteCache = SQLiteCache.getInstance();
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -48,23 +50,60 @@ public class SQLiteCacheTest {
     /**
      * Test of insertImage method, of class SQLiteCache.
      */
+//    @Test
+//    public void testInsertImage() throws MalformedURLException, IOException {
+//        System.out.println("insertImage");
+//        URL url = new URL("http://a.tile.openstreetmap.org/12/785/1219.png");
+//        BufferedImage img = ImageIO.read(url);
+//        sqliteCache.insertImage(url, img);
+//    }
+//
+//    /**
+//     * Test of fetchImage method, of class SQLiteCache.
+//     */
+//    @Test
+//    public void testFetchImage() throws IOException {
+//        System.out.println("fetchImage");
+//        URL url = new URL("http://a.tile.openstreetmap.org/12/785/1219.png");
+//        BufferedImage img = sqliteCache.fetchImage(url);
+//        File f = new File("C:/tmp/sqliteimg.png");
+//        ImageIO.write(img, "png", f);
+//    }
+    /**
+     * Test of put method, of class SQLiteCache.
+     */
     @Test
-    public void testInsertImage() throws MalformedURLException, IOException {
-        System.out.println("insertImage");
-        URL url = new URL("http://a.tile.openstreetmap.org/12/785/1219.png");
-        BufferedImage img = ImageIO.read(url);
-        sqliteCache.insertImage(url, img);
+    public void testPut() throws IOException {
+        System.out.println("put");
+        URL url;
+        try {
+            url = new URL("http://a.tile.openstreetmap.org/12/785/1219.png");
+
+            TileSet tileSet = new HTTPTileSet("http://a.tile.openstreetmap.org/{z}/{x}/{y}.png");
+            Tile t = tileSet.getTile(12, 785, 1219);
+            sqliteCache.put(url, t);
+
+            t = tileSet.getTile(12, 785, 1218);
+            t.fetch();
+            sqliteCache.put(url, t);
+
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(SQLiteCacheTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     /**
-     * Test of fetchImage method, of class SQLiteCache.
+     * Test of get method, of class SQLiteCache.
      */
     @Test
-    public void testFetchImage() throws IOException {
-        System.out.println("fetchImage");
+    public void testGet() throws MalformedURLException, IOException {
         URL url = new URL("http://a.tile.openstreetmap.org/12/785/1219.png");
-        BufferedImage img = sqliteCache.fetchImage(url);
-        File f = new File("C:/tmp/sqliteimg.png");
-        ImageIO.write(img, "png", f);
+
+        TileSet tileSet = new HTTPTileSet("http://a.tile.openstreetmap.org/{z}/{x}/{y}.png");
+        Tile tile = sqliteCache.get(url, tileSet);
+        ImageTile imageTile = (ImageTile)tile;
+        BufferedImage image = imageTile.fetch();
+        ImageIO.write(image, "png", new File("test-output/testGet.png"));
     }
 }
