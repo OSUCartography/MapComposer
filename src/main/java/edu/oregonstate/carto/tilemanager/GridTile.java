6325768db3d93d16f385c33dc8405a376247ecaf
Bernie Jenny <jennyb@geo.oregonstate.edu>
2014-09-24 15:15:56 -0700

    Fixed bug in GridTile.createMegaTile

--- src/main/java/edu/oregonstate/carto/tilemanager/GridTile.java
+++ src/main/java/edu/oregonstate/carto/tilemanager/GridTile.java
@@ -63,7 +63,7 @@ public class GridTile extends Tile<Grid> {
         double west = inStream.readDouble();
         double north = inStream.readDouble();
         float[][] g = new float[rows][cols];
-        for (float [] row : g) {
+        for (float[] row : g) {
             for (int col = 0; col < cols; col++) {
                 row[col] = inStream.readFloat();
             }
@@ -72,7 +72,7 @@ public class GridTile extends Tile<Grid> {
         grid.setWest(west);
         grid.setNorth(north);
     }
-    
+
     /**
      * Fetches the tile's grid data from memory, http, or file. This may block
      * the calling thread until the grid is loaded.
@@ -86,11 +86,11 @@ public class GridTile extends Tile<Grid> {
             TileSet tileSet = getTileSet();
             URL url = tileSet.urlForTile(this);
             grid = BinaryGridReader.read(url);
-            
+
             // cell size at equator for given zoom level
             double cellSize = (2 * Math.PI * 6378137) / (TILE_SIZE * Math.pow(2, getZ()));
             grid.setCellSize(cellSize);
-            
+
             tileSet.tileChanged(this);
         }
         return grid;
@@ -105,60 +105,104 @@ public class GridTile extends Tile<Grid> {
     @Override
     protected void toBinary(DataOutputStream out) throws IOException {
         super.toBinary(out);
-        
+
         out.writeInt(grid.getCols());
         out.writeInt(grid.getRows());
         out.writeDouble(grid.getCellSize());
         out.writeDouble(grid.getWest());
         out.writeDouble(grid.getNorth());
-                
+
         float[][] g = grid.getGrid();
-        for (float [] row : g) {
+        for (float[] row : g) {
             for (float v : row) {
                 out.writeFloat(v);
             }
         }
-        
+
         out.flush();
     }
-    
+
     public Grid createMegaTile() throws IOException {
 
-        Grid topLeftGrid = ((GridTile) getTopLeftTile()).fetch();
-        Grid topGrid = ((GridTile) getTopTile()).fetch();
-        Grid topRightGrid = ((GridTile) getTopRightTile()).fetch();
-        Grid rightGrid = ((GridTile) getRightTile()).fetch();
-        Grid bottomRightGrid = ((GridTile) getBottomRightTile()).fetch();
-        Grid bottomGrid = ((GridTile) getBottomTile()).fetch();
-        Grid bottomLeftGrid = ((GridTile) getBottomLeftTile()).fetch();
-        Grid leftGrid = ((GridTile) getLeftTile()).fetch();
-        Grid centerGrid = fetch();
-
-        int tileRows = centerGrid.getRows();
-        int tileCols = centerGrid.getCols();
-        int megaTileSize = tileRows * 3;
+        int tileRows = TILE_SIZE;
+        int tileCols = TILE_SIZE;
+        int megaTileSize = TILE_SIZE * 3;
         float[][] mergedArray = new float[megaTileSize][megaTileSize];
-        for (int r = 0; r < tileRows; r++) {
-            System.arraycopy(topLeftGrid.getGrid()[r], 0, mergedArray[r], 0, tileCols);
-            System.arraycopy(topGrid.getGrid()[r], 0, mergedArray[r], tileCols, tileCols);
-            System.arraycopy(topRightGrid.getGrid()[r], 0, mergedArray[r], tileCols * 2, tileCols);
+        try {
+            Grid topLeftGrid = ((GridTile) getTopLeftTile()).fetch();
+            for (int r = 0; r < tileRows; r++) {
+                System.arraycopy(topLeftGrid.getGrid()[r], 0, mergedArray[r], 0, tileCols);
+            }
+        } catch (IOException ex) {
         }
 
-        for (int r = 0; r < tileRows; r++) {
-            System.arraycopy(leftGrid.getGrid()[r], 0, mergedArray[r + tileRows], 0, tileCols);
-            System.arraycopy(centerGrid.getGrid()[r], 0, mergedArray[r + tileRows], tileCols, tileCols);
-            System.arraycopy(rightGrid.getGrid()[r], 0, mergedArray[r + tileRows], tileCols * 2, tileCols);
+        try {
+            Grid topGrid = ((GridTile) getTopTile()).fetch();
+            for (int r = 0; r < tileRows; r++) {
+                System.arraycopy(topGrid.getGrid()[r], 0, mergedArray[r], tileCols, tileCols);
+            }
+        } catch (IOException ex) {
         }
 
-        for (int r = 0; r < tileRows; r++) {
-            System.arraycopy(bottomLeftGrid.getGrid()[r], 0, mergedArray[r + tileRows * 2], 0, tileCols);
-            System.arraycopy(bottomGrid.getGrid()[r], 0, mergedArray[r + tileRows * 2], tileCols, tileCols);
-            System.arraycopy(bottomRightGrid.getGrid()[r], 0, mergedArray[r + tileRows * 2], tileCols * 2, tileCols);
+        try {
+            Grid topRightGrid = ((GridTile) getTopRightTile()).fetch();
+            for (int r = 0; r < tileRows; r++) {
+                System.arraycopy(topRightGrid.getGrid()[r], 0, mergedArray[r], tileCols * 2, tileCols);
+            }
+        } catch (IOException ex) {
         }
 
-        Grid mergedGrid = new Grid(mergedArray, topGrid.getCellSize());
-        mergedGrid.setWest(topLeftGrid.getWest());
-        mergedGrid.setNorth(topLeftGrid.getNorth());
+        try {
+            Grid leftGrid = ((GridTile) getLeftTile()).fetch();
+            for (int r = 0; r < tileRows; r++) {
+                System.arraycopy(leftGrid.getGrid()[r], 0, mergedArray[r + tileRows], 0, tileCols);
+            }
+        } catch (IOException ex) {
+        }
+
+        try {
+            Grid centerGrid = fetch();
+            for (int r = 0; r < tileRows; r++) {
+                System.arraycopy(centerGrid.getGrid()[r], 0, mergedArray[r + tileRows], tileCols, tileCols);
+            }
+        } catch (IOException ex) {
+        }
+
+        try {
+            Grid rightGrid = ((GridTile) getRightTile()).fetch();
+            for (int r = 0; r < tileRows; r++) {
+                System.arraycopy(rightGrid.getGrid()[r], 0, mergedArray[r + tileRows], tileCols * 2, tileCols);
+            }
+        } catch (IOException ex) {
+        }
+
+        try {
+            Grid bottomLeftGrid = ((GridTile) getBottomLeftTile()).fetch();
+            for (int r = 0; r < tileRows; r++) {
+                System.arraycopy(bottomLeftGrid.getGrid()[r], 0, mergedArray[r + tileRows * 2], 0, tileCols);
+            }
+        } catch (IOException ex) {
+        }
+
+        try {
+            Grid bottomGrid = ((GridTile) getBottomTile()).fetch();
+            for (int r = 0; r < tileRows; r++) {
+                System.arraycopy(bottomGrid.getGrid()[r], 0, mergedArray[r + tileRows * 2], tileCols, tileCols);
+            }
+        } catch (IOException ex) {
+        }
+
+        try {
+            Grid bottomRightGrid = ((GridTile) getBottomRightTile()).fetch();
+            for (int r = 0; r < tileRows; r++) {
+                System.arraycopy(bottomRightGrid.getGrid()[r], 0, mergedArray[r + tileRows * 2], tileCols * 2, tileCols);
+            }
+        } catch (IOException ex) {
+        }
+        
+        Grid mergedGrid = new Grid(mergedArray, fetch().getCellSize());
+        mergedGrid.setWest(fetch().getWest());
+        mergedGrid.setNorth(fetch().getNorth());
 
         return mergedGrid;
     }
